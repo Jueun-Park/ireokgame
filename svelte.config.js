b740c6ea57bacfcd1f75a9014965919c06787001
Jueun-Park <elpion19@gmail.com>
2023-11-19 22:32:18 +0900

    #12 base path config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 	}
 };
-
-config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 
 export default config;
